526706a94d2075d6a611afedb67b1ed0de35cd69
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2024-11-04 21:18:40 +0700

    Update VC.py

--- VC.py
+++ VC.py
@@ -1,37 +1,29 @@
 import streamlit as st
 import util
 
-def main():
-        
+def main(pesan, key):
+    # Memastikan kunci sama panjang atau lebih panjang dari pesan
     if len(key) <= len(pesan):
-        big_key = key * (len(pesan) // len(key)) + key[:len(pesan) % len(key)]
+        # Memperluas kunci menjadi ukuran yang sama dengan pesan
+        big_key = (key * (len(pesan) // len(key))) + key[:len(pesan) % len(key)]
         text_encrypt = util.encrypt(pesan, big_key)
         text_decrypt = util.decrypt(text_encrypt, big_key)
         
-        st.header('#========Start Encrypt=======#') 
+        st.header('#======== Start Encrypt =======#') 
         st.write('Encryption Text       : ' + text_encrypt)
        
-        st.header('#========Start Decrypt=======#')
+        st.header('#======== Start Decrypt =======#')
         st.write('Descryption Text      : ' + text_decrypt)
         
     else:
-        st.write('Error: len(key)>len(text) ')
+        st.write('Error: len(key) > len(text)')
                
-
-        
-     
-    
 if __name__ == '__main__':
-    st.title("Program Encyption and Decryption Vigenere Chiper")
+    st.title("Program Encryption and Decryption Vigenere Cipher")
     form = st.form(key='my-form')
     pesan = form.text_input('Silakan Masukan Text Anda)', '-')
     key = form.text_input('Silakan Masukan Key Anda)', '-')
     submit = form.form_submit_button('Generate')
-    
    
     if submit:
-        main()
-       
-                        
-    
-     
+        main(pesan, key)
