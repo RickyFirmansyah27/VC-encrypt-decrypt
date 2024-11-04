936d977d98031bb08be3d36b2cc311fac42f372e
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2024-11-04 21:31:44 +0700

    Update util.py

--- util.py
+++ util.py
@@ -1,11 +1,26 @@
 from pycipher import Vigenere
 
-def encrypt(text, key):
-    # Menggunakan fungsi encrypt dari Vigenere
+def caesar_encrypt(text, shift):
+    result = ''
+    for char in text:
+        if char.isalpha():
+            shift_amount = shift % 26
+            shifted = ord(char) + shift_amount
+            if char.islower():
+                result += chr(shifted) if shifted <= ord('z') else chr(shifted - 26)
+            else:
+                result += chr(shifted) if shifted <= ord('Z') else chr(shifted - 26)
+        else:
+            result += char
+    return result
+
+def caesar_decrypt(text, shift):
+    return caesar_encrypt(text, -shift)
+
+def vigenere_encrypt(text, key):
     cipher = Vigenere(key)
     return cipher.encipher(text)
 
-def decrypt(text, key):
-    # Menggunakan fungsi decrypt dari Vigenere
+def vigenere_decrypt(text, key):
     cipher = Vigenere(key)
     return cipher.decipher(text)
